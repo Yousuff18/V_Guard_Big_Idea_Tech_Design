7ba68bd667f05d34c8f18ab3771f925dc1c0eca8
Yousuff <yousuffcse@gmail.com>
2025-08-09 22:39:02 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// /data/app.js
+// Main front-end JavaScript for ESP32 Recipe Controller
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Elements
+  const hamburger = document.getElementById("hamburger");
+  const sideNav
