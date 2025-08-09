4b0a5f34d33843a9b0baf59c6af686f7a58e7bef
Yousuff <yousuffcse@gmail.com>
2025-08-09 22:45:42 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-// /data/app.js
-// Main front-end JavaScript for ESP32 Recipe Controller
-
-document.addEventListener("DOMContentLoaded", () => {
-  // Elements
-  const hamburger = document.getElementById("hamburger");
-  const sideNav
